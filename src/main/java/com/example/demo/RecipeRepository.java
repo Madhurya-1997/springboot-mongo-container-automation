6a18e97b8e383f4b7b34b61efd5782aac92d6a4d
Madhurya Bharadwaaz <dg.hipl@gmail.com>
2022-08-10 15:37:01 +0530

    resolved temporary errors

--- src/main/java/com/example/demo/RecipeRepository.java
+++ src/main/java/com/example/demo/RecipeRepository.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 //@Repository
 //public interface RecipeRepository extends MongoRepository<Recipe, String> {
